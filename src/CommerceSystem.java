09fda9d09df84345cc0645e7c21da98df2ef9ccf
KYJ <nananan1213@gmail.com>
2025-12-15 11:16:14 +0900

    feat: step 2 수행

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,2 +1,35 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class CommerceSystem {
+    List<Product> products;
+
+    public CommerceSystem(List<Product> products) {
+        this.products = products;
+    }
+
+    public void start() {
+        String selectMenu;
+        StringBuilder menuStrBuilder = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+
+        //region 메뉴 출력 및 입력
+        menuStrBuilder.append("[ 실시간 커머스 플랫폼 - 전자제품 ]\n");
+        int index = 1;
+        for(Product product : products) {
+            menuStrBuilder.append(index).append('.').append(product.printInfoButNotQty()).append("\n");
+            if(index == products.size()) {
+                menuStrBuilder.append("0.").append(String.format(" %-15s | %-10s", "exit", "프로그램 종료"));
+            }
+            index++;
+        }
+        System.out.println(menuStrBuilder);
+        System.out.print("메뉴 번호를 입력해주세요 : ");
+        selectMenu = scanner.nextLine();
+
+        if(selectMenu.equals("0")) {
+            return;
+        }
+        //endregion
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String selectMenu;
-        StringBuilder menuStrBuilder = new StringBuilder();
+        //region CommerceSystem 객체 생성 및 초기화 수행
         List<Product> products = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-
         Object[][] productInfoArr = {
                 {"Galaxy S26", 1170000, "최신 Android 스마트폰", 100}
                 , {"iPhone 18", 1300000, "최신 IOS 스마트폰", 100}
@@ -27,22 +23,9 @@ public class Main {
             );
         }
 
-        menuStrBuilder.append("[ 실시간 커머스 플랫폼 - 전자제품 ]\n");
-        int index = 1;
-        for(Product product : products) {
-            menuStrBuilder.append(index).append('.').append(product.printInfoButNotQty()).append("\n");
-            if(index == products.size()) {
-                menuStrBuilder.append("0.").append(String.format(" %-15s | %-10s", "exit", "프로그램 종료"));
-            }
-            index++;
-        }
-        System.out.println(menuStrBuilder);
-        System.out.print("메뉴 번호를 입력해주세요 : ");
-        selectMenu = scanner.nextLine();
+        CommerceSystem commerceSystem = new CommerceSystem(products);
+        //endregion
 
-        if(selectMenu.equals("0")) {
-            return;
-        }
-
+        commerceSystem.start();
     }
 }
